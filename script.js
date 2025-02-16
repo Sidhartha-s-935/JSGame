b5fb2a72446459ea10794027f502d4434a3929a4
Sidhartha S <darksoul13570@gmail.com>
2025-02-16 21:27:06 +0530

    feat:player anim
    Player moment and animation

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.addEventListener('load',function(){
                 this.game.lastkey = e.key;
             });
             window.addEventListener('keyup', (e) => {
-                this.game.lastkey = undefined;
+                this.game.lastkey = 'R' + e.key;
             });
         }
     }
@@ -23,15 +23,19 @@ window.addEventListener('load',function(){
             this.spriteheight = 126; 
             this.frameX = 0;
             this.frameY = 0;
+            this.lastYframe = undefined;
+            this.maxframeX = 5;
+            this.maxframeY = 7;
             this.x = 10;
             this.y = 70;
             this.width = this.spritewidth;
             this.height = this.spriteheight;
             this.speedX = 0;
             this.speedY = 0;
-            this.topspeed = 3;
+            this.topspeed = 4;
             this.topmargin = 45;
             this.image = document.getElementById("player");
+            this.moving = false;
         }
         draw(context){
             context.drawImage(this.image, this.frameX * this.spritewidth, this.frameY * this.spriteheight, this.spritewidth, this.spriteheight, this.x, this.y, this.width, this.height);
@@ -43,29 +47,59 @@ window.addEventListener('load',function(){
         update(){
             if(this.game.lastkey === 'ArrowUp'){
                 this.setSpeed(0, -this.topspeed);
+                this.frameY = 0;
+                this.moving = true;
+            }else if(this.game.lastkey === 'RArrowUp'){
+                this.setSpeed(0, 0);
+                this.moving = false;
             }else if(this.game.lastkey === 'ArrowDown'){
                 this.setSpeed(0, this.topspeed);
+                this.frameY = 2;
+                this.moving = true;
+            }else if(this.game.lastkey === 'RArrowDown'){
+                this.setSpeed(0, 0);
+                this.moving = false;
             }else if(this.game.lastkey === 'ArrowLeft'){
                 this.setSpeed(-this.topspeed, 0);
+                this.frameY = 1;
+                this.moving = true;
+            }else if(this.game.lastkey === 'RArrowLeft'){
+                this.setSpeed(0, 0);
+                this.frameY = 1;
+                this.moving = false;
             }else if(this.game.lastkey === 'ArrowRight'){
                 this.setSpeed(this.topspeed, 0);
-            }
-            else{
+                this.frameY = 3;
+                this.moving = true;
+            }else if(this.game.lastkey === 'RArrowRight'){
+                this.setSpeed(0, 0);
+                this.frameY = 3;
+                this.moving = false;
+            }else{
                 this.setSpeed(0, 0);
             }
             this.x += this.speedX;
             this.y += this.speedY;
+            // Check if player is out of bounds X value
             if (this.x < 0){
                 this.x = 0;
             }
             if(this.x > this.game.width-this.width){
                 this.x = this.game.width-this.width;
             }
+            // Check if player is out of bounds Y value
             if (this.y < this.topmargin){
                 this.y = this.topmargin;
             }
             if(this.y > this.game.height-this.height){
                 this.y = this.game.height-this.height;
+            }
+            //Animation
+            if(this.frameX < this.maxframeX && this.moving){
+                this.frameX++;
+            }
+            else{
+                this.frameX = 0;
             }
         }
     }
